f512c4a322b6ed798f2a67c0a34fc6994ef1f13e
Andrew Winnicki <andrew.winnicki.sc@gmail.com>
2018-10-18 13:13:32 -0400

    added comments to capture and thresh programs

--- capture.py
+++ capture.py
@@ -1,18 +1,22 @@
 import cv2
 
+#initialize variables
 cameraPort = 0
 
 calibrateFrames = 30
 
+#make camera object
 camera = cv2.VideoCapture(cameraPort)
 
+#define getimage function
 def getImage():
     retval, im = camera.read()
     return im
-
+#calibrate camera (take 30 pictures in a row, no save)
 for frame in range(calibrateFrames):
     temp = getImage()
 
+#take image and save
 print("Taking image..........NOW!!!!!")
 
 image = getImage()
--- thresh.py
+++ thresh.py
@@ -1,11 +1,14 @@
 import cv2
 
+#store image using imread() method (from cv2 library) in img variable
 img = cv2.imread('/Users/andrewwinnicki/desktop/camera.png', 0)
 
+#threshold the image (built-in cv2 method)
 ret, threshed = cv2.threshold(img, 115, 255, cv2.THRESH_BINARY)
 #for mobile screen, lower thresh = 215 works well
 #for paper, lower thresh = 
 
+#print and save
 print('Thresholded image...')
 
 file = '/Users/andrewwinnicki/desktop/image.png'
